4deb87b53b32316220949db7ae9cf0dd7390ad14
kAgotsi <cyrilleagotsi@gmail.com>
2021-06-29 08:02:08 +0200

    Init project

---
+++ src/main/java/com/atos/airfrance/codingame/CodingameApplication.java
@@ -0,0 +1,13 @@
+package com.atos.airfrance.codingame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingameApplication.class, args);
+	}
+
+}
